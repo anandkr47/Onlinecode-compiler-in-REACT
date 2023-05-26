9d1eac244e9489cd34ba5a987f247659c90f1b3c
Anand kumar <anandkumar.trident@gmail.com>
2023-05-27 00:16:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function compile() {
 	}
 
 	// Post request to compile endpoint
-	Axios.post(`https://compiler-api.onrender.com/compile`, {
+	Axios.post(`http://localhost:8000/compile`, {
 	code: userCode,
 	language: userLang,
 	input: userInput }).then((res) => {
@@ -53,7 +53,6 @@ function compile() {
 	setLoading(false);
 	})
 }
-
 	
 
 // Function to clear the output screen
