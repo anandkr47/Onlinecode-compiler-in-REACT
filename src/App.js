d92c1f6a142435ad479fefa1accfdf8c565c8029
Anand kumar <anandkumar.trident@gmail.com>
2023-05-28 22:02:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function compile() {
 	}
 
 	// Post request to compile endpoint
-	Axios.post(`http://localhost:8000/compile`, {
+	Axios.post(`/compile`, {
 	code: userCode,
 	language: userLang,
 	input: userInput }).then((res) => {
